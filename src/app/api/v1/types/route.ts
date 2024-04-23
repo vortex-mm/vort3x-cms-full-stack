3e3288c6623be933d95b28d310a19f8ac848287d
robertni <robert@3sam.io>
2024-04-23 16:15:36 +0700

    feat: integrate with prisma and neon with next-auth

--- src/app/api/v1/types/route.ts
+++ src/app/api/v1/types/route.ts
@@ -1,22 +1,21 @@
 import { NextApiRequest } from 'next'
 import { NextResponse } from 'next/server'
-import { withDatabase, releaseDatabase } from '@/app/api/v1/middleware/database'
 import { PoolClient } from '@neondatabase/serverless'
 
-export default async function GET(req: CustomNextApiRequest) {
+export async function GET(req: CustomNextApiRequest) {
   try {
+    console.log({ req })
+    // prisma.ty
     // Execute a query using the database client attached to the request object
-    const queryResult = await req.db.query('SELECT * FROM your_table')
+    // const queryResult = await req.db.query('SELECT * FROM types')
 
     // Return the query result as JSON response
-    return NextResponse.json(queryResult.rows)
+    return NextResponse.json({})
   } catch (error) {
     console.error('Error:', error)
     return NextResponse.json({ error: 'Internal Server Error' })
   }
 }
-
-export const middleware = [withDatabase, releaseDatabase]
 
 interface CustomNextApiRequest extends NextApiRequest {
   db: PoolClient
